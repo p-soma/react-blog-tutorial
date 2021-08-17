890d4cf07477635fd3d07450be44909d07f5d438
Paul Soma <46449481+p-soma@users.noreply.github.com>
2021-08-16 21:45:27 -0400

    Through tutorial 4

--- src/psoma-blog/src/App.js
+++ src/psoma-blog/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const person = {name : 'yoshi', age : 30};
+  const link = "https://www.google.com";
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+
+        <p>{"Hello ninjas"}</p>
+        <p>{10}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random() + 2 }</p>
+
+        <a href={link}>The google site</a>
       </div> 
     </div>
   );
