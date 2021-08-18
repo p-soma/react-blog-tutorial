427ac231b2af3b67b85e716754a6beb0de05f463
Paul Soma <46449481+p-soma@users.noreply.github.com>
2021-08-18 19:35:41 -0400

    Through tutorial 9

--- src/psoma-blog/src/Home.js
+++ src/psoma-blog/src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return (  
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+  // let name = 'Mario';
+
+  const handleClick = (e) => {
+    setName('luigi');
+    setAge(30);
+  }
+
+  return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <p>{name} is {age} years old</p>
     </div>
   );
 }
- 
+
 export default Home;
