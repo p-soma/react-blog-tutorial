154afeae0e02008024922d68c9f26d5f89c7702d
Paul Soma <46449481+p-soma@users.noreply.github.com>
2021-08-18 21:41:56 -0400

    through 15

--- src/psoma-blog/src/Home.js
+++ src/psoma-blog/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
+
+  const [name, setName] = useState('mario');
   
   const handleDelete = (id) => {
     const newBlogs = blogs.filter( b => b.id !== id);
@@ -17,12 +19,15 @@ const Home = () => {
   // useEffect() runs every time we rerender
   useEffect( () => {
     console.log('use effect ran');
-  });
+    console.log({name});
+  }, [name]); // dependency array, only run useEffect on first render (refresh) and for changes in state of dep objs in array
 
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
   );
 }
