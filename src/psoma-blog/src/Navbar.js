088784943e89a73f911c86efb56e25e213e554e5
Paul Soma <46449481+p-soma@users.noreply.github.com>
2021-08-22 13:05:05 -0400

    Completed tutorial

--- src/psoma-blog/src/Navbar.js
+++ src/psoma-blog/src/Navbar.js
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <div className="SiteTitle">
+        <Link to='/'>
+          <h1>The Dojo Blog</h1>
+        </Link>
+      </div>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
